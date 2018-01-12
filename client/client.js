81e7ff6af2425157e3479998cc42e642702c4fbe
David Ko <david.ko@pvtmethod.com>
2018-01-12 17:09:33 -0500

    wip

--- client/client.js
+++ client/client.js
@@ -9,10 +9,10 @@ ws.onmessage = function(evt) {
 let textarea = document.querySelector('.counter');
 
 let doc = connection.get('notepads', 'mainOne');
-doc.fetch(data => {
+doc.subscribe(data => {
   console.log(data);
 });
-console.log(doc);
+console.log(doc.data);
 doc.subscribe((err) => {
   console.log(err);
   console.log(doc.data);
