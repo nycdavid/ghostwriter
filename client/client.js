e956dc64ce3bafbea7ae94101f97ac1ad738724b
David Ko <david.ko@pvtmethod.com>
2018-01-25 12:43:53 -0500

    Changes to detect cursor position

--- client/client.js
+++ client/client.js
@@ -9,17 +9,21 @@ doc.subscribe(setNewData);
 doc.on('op', setNewData);
 
 function setNewData() {
-  let position = textarea.selectionStart;
+  let position = parseInt(localStorage.getItem('cursorPosition'), 10);
   textarea.textContent = doc.data.blogPost;
   textarea.selectionStart = position + 1;
+  textarea.selectionEnd = position + 1;
+  localStorage.setItem('cursorPosition', position);
 }
 
 textarea.onkeypress = function register(evt) {
   let position = evt.target.selectionStart;
+  localStorage.setItem('cursorPosition', position);
   if (evt.key === 'Enter') {
     evt.preventDefault();
     return doc.submitOp([{ p: ['blogPost', position], si: "\n" }]);
   }
+  // Registering a regular character
   doc.submitOp([{ p: ['blogPost', position], si: evt.key }])
   evt.preventDefault();
 }
@@ -28,6 +32,10 @@ textarea.addEventListener('pasteEvt', (evt) => {
   let cont = document.querySelector('.counter').value;
   console.log(cont);
 });
+
+textarea.onclick = evt => {
+  console.log(evt);
+}
 
 textarea.onkeyup = evt => {
   let cont = document.querySelector('.counter').value;
